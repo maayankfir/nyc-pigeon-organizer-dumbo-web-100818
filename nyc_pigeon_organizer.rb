602700bc28342c83c934d7cddae4ec0b6cc0104c
Maayan Kfir <mayankfir@gmail.com>
2018-09-30 19:37:44 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |key, value|
+    value.each do |k, v|
+      v.each do |name|
+        if !list.has_key?(name)
+          list[name] = {}
+        end
+        if !list[name].has_key?(key)
+          list[name][key] = []
+        end
+        if !list[name][key].include?(k)
+          list[name][key] << k.to_s
+        end
+      end
+    end
+  end
+  list
 end
